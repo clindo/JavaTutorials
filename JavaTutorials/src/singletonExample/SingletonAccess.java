3d5aa2ec45f7c1973655777ab47ee22c2007b579
Clindo <clinddo@gmail.com>
2016-11-12 18:01:05 +0100

    added object of Classic Singleton as well

--- JavaTutorials/src/singletonExample/SingletonAccess.java
+++ JavaTutorials/src/singletonExample/SingletonAccess.java
@@ -5,5 +5,10 @@ public class SingletonAccess {
 	public static void main(String[] args) {
 		Singleton tmp = Singleton.getInstance();
 		tmp.demoMethod();
+		
+		ClassicSingleton test = ClassicSingleton.getInstance();
+		test.demoMethod();
+		
+		 
 	}
 }
